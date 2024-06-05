217a4bf69bd7fa7bf72055bbf482e9e099ce40c6
unknown <Imran>
2024-06-05 12:12:19 +0600

    add about page and it's Hero section

---
+++ nodes-theme/app/about/page.tsx
@@ -0,0 +1,20 @@
+import Hero from "../../components/about/Hero"
+
+
+function Aboutpage() {
+  return (
+    <div className="nt-page-about">
+    <section className="nt-section nt-hero">
+      <div className="nt-row">
+        <div className="nt-col nt-hero-A">
+          <Hero />
+        </div>
+      </div>
+    </section>
+    {/* End Hero */}
+    </div>
+  )
+}
+
+export default Aboutpage
+
