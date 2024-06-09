0ca7b782a2e2878385dee6f27ee1a7b7ad2e2787
unknown <Imran>
2024-06-09 14:06:02 +0600

    add Card section in elements page

--- nodes-theme/app/elements/page.tsx
+++ nodes-theme/app/elements/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Hero from "../../components/elements/Hero";
 import TaskSection from "../../components/TaskSection";
+import DocuVerticalImg from "../../public/docu-vertical.png";
+import DocuHorizontal from "../../public/docu-horizontal.png";
+import DocumentationCard from "../../components/DocumentationCard";
+import ProgramCard from "../../components/ProgramCard";
+import HeroCard from "../../components/about/HeroCard";
 
 function ElementsPage() {
   return (
@@ -21,6 +26,22 @@ function ElementsPage() {
         </div>
       </section>
       {/* End Tasks */}
+      <section className="nt-section nt-cards">
+        <div className="nt-row">
+          <div className="nt-col nt-card-A">
+          <div className="flex gap-6 justify-center">
+            <DocumentationCard path={DocuHorizontal} size={"384"} />
+            <DocumentationCard path={DocuVerticalImg} size={"282"} />
+            <DocumentationCard path={DocuHorizontal} size={"486"} />
+          </div>
+          <div className="flex nt-max-container gap-6">
+            <ProgramCard />
+            <HeroCard />
+          </div>
+          </div>
+        </div>
+      </section>
+      {/* End Cards */}
     </div>
   );
 }
