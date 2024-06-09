2d3f14d50af654026ddb3f8d3795cc5c8dd53626
unknown <Imran>
2024-06-09 11:56:00 +0600

    add Hero section in elements page

--- nodes-theme/app/elements/page.tsx
+++ nodes-theme/app/elements/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Hero from "../../components/elements/Hero";
 
 function ElementsPage() {
-  return <div>ElementsPage</div>;
+  return (
+    <div className="nt-page-about">
+      <section className="nt-section nt-hero">
+        <div className="nt-row">
+          <div className="nt-col nt-hero-A">
+            <Hero />
+          </div>
+        </div>
+      </section>
+      {/* End Hero */}
+    </div>
+  );
 }
 
 export default ElementsPage;
